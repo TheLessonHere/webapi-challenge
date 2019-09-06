bba35aedf7c0831668132e4020126b842dbdf7b7
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-09-06 09:32:57 -0600

    Initial commit with dependencies and server set up

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const server = require('./server');
+
+// const userRouter = require('./users/userRouter');
+// const postRouter = require('./posts/postRouter');
+// server.use('/api/users', userRouter);
+// server.use('/api/posts', postRouter);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
